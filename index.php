6116d5730e45dd8aacdef552fdda65a436435dd0
sophieanne3git <sophieannepro@gmail.com>
2024-05-14 01:00:13 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,27 @@
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hello</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+
+    <video autoplay muted loop id="background-video">
+        <source src="/src/animal.mp4" type="video/mp4">
+    </video>
+
+    <h1 class="title_landingpage">Hello Lionel !</h1>
+    <div class="landing_container">
+        <p class="txt_needhelp">J'ai besoin d'aide pour ma connexion à une base de données OVH , connecté avec PhpmyAmdmin ,
+            ma connection en PHP objet ,ne marche pas donc mon utilisateur n'a pas accès au coté admin du site web.
+            Est ce posside de m'appeler ou m'ajouter sur Discord ? S'il te plait ? 
+            Je dois rendre mon site internet pour Jeudi...</p>
+        <a href="/contact.php"  rel="noopener noreferrer" class="link_discord">Contact</a>
+    </div>
+
+</body>
+
+</html>
